f35f330355e401c12e9ca7c4e1eca7a6f934cf25
Meydiva-IY <meydivaintayeza@gmail.com>
2024-05-21 20:03:56 +0700

    views/dashboard.ejsviews/editprofil.ejsviews/login.ejsviews/profile.ejsviews/sidebar.ejs

--- routes/dist/index.dev.js
+++ routes/dist/index.dev.js
@@ -10,5 +10,25 @@ router.get('/', function (req, res, next) {
     title: 'Express'
   });
 });
+router.get('/login', function (req, res, next) {
+  res.render('login', {
+    title: 'Login'
+  });
+});
+router.get('/sidebar', function (req, res, next) {
+  res.render('sidebar', {
+    title: 'sidebar'
+  });
+});
+router.get('/profile', function (req, res, next) {
+  res.render('profile', {
+    title: 'profile'
+  });
+});
+router.get('/editprofile', function (req, res, next) {
+  res.render('editprofil', {
+    title: 'EditProfile'
+  });
+});
 module.exports = router;
 //# sourceMappingURL=index.dev.js.map
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,12 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
-
+router.get('/sidebar', function(req, res, next) {
+  res.render('sidebar', { title: 'sidebar' });
+});
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'profile' });
+});
 router.get('/editprofile', function(req, res, next) {
   res.render('editprofil', { title: 'EditProfile' });
 });
