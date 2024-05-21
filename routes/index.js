ebd58fc824947ebeb6e2e3e87ef63b64ac1a6d89
reginanathamiya08 <reginanathamiya08@gmail.com>
2024-05-21 18:36:43 +0700

    profile siaap

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,17 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
-router.get('/editprofile', function(req, res, next) {
-  res.render('editprofil', { title: 'EditProfile' });
+
+router.get('/sidebar', function(req, res, next) {
+  res.render('sidebar', { title: 'sidebar' });
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'profile' });
+});
+
+router.get('/editprofil', function(req, res, next) {
+  res.render('editprofil', { title: 'editprofil' });
 });
 
 module.exports = router;
