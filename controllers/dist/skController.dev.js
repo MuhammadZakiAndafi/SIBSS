552ab6976180f934e9c42db0e82555432a2622b8
Meydiva-IY <meydivaintayeza@gmail.com>
2024-06-23 22:53:25 +0700

    skedit

--- controllers/dist/skController.dev.js
+++ controllers/dist/skController.dev.js
@@ -117,7 +117,43 @@ exports.generateSK = function _callee2(req, res) {
             doc.text("Tanggal: ".concat(pengajuan.SuratKeputusan.tanggal.toDateString()));
           } else {
             doc.text("Nomor Surat: -");
-            doc.text("Tanggal: -");
+            doc.text("Tanggal: -"); // Add title
+
+            doc.text('KEPUTUSAN REKTOR UNIVERSITAS ANDALAS', {
+              fontSize: 24,
+              fontWeight: 'bold'
+            });
+            doc.text('Nomor 1084/XIV/R/KPT/2024'); // Add subtitle
+
+            doc.text('BERHENTI STUDI SEMENTARA MAHASISWA PROGRAM SARJANA UNIVERSITAS ANDALAS PADA SEMESTER GENAP TAHUN AKADEMIK 2023/2024', {
+              fontSize: 18,
+              fontWeight: 'bold'
+            });
+            doc.text('REKTOR UNIVERSITAS ANDALAS,', {
+              fontSize: 18,
+              fontWeight: 'bold'
+            }); // Add content
+
+            doc.text('Membaca:', {
+              fontSize: 14,
+              fontWeight: 'bold'
+            });
+            doc.text("Surat Permohonan Berhenti Studi Sementara mahasiswa Program Sarjana Universitas Andalas atas nama ".concat(pengajuan.name, " NIM. ").concat(pengajuan.nim, " dan kawan-kawan yang telah disetujui Dekan/Direktur Fakultas/Program di lingkungan Universitas Andalas"));
+            doc.text('Menimbang:', {
+              fontSize: 14,
+              fontWeight: 'bold'
+            });
+            doc.text("a. bahwa berdasarkan Peraturan Rektor Nomor 3 Tahun 2016 tentang peraturan akademik Universitas Andalas, kepada ".concat(pengajuan.name, " NIM. ").concat(pengajuan.nim, " dan kawan-kawan dapat diberikan izin untuk Berhenti Studi Sementara;"));
+            doc.text('b. bahwa untuk pelaksanaan hal tersebut pada butir a di atas perlu ditetapkan dengan Keputusan Rektor.');
+            doc.text('Mengingat:', {
+              fontSize: 14,
+              fontWeight: 'bold'
+            });
+            doc.text('1. Undang-Undang Nomor 12 Tahun 2012 tentang Pendidikan Tinggi (Lembaran Negara Republik Indonesia Tahun 2012 Nomor 158, Tambahan Lembaran Negara Republik Indonesia Nomor 5336);');
+            doc.text('2. Peraturan Pemerintah Nomor 4 Tahun 2014 tentang Penyelenggaraan Pendidikan Tinggi dan Pengelolaan Perguruan Tinggi (Lembaran Negara Republik Indonesia Tahun 2014 Nomor 14, Tambahan Lembaran Negara Republik Indonesia Nomor 5500);');
+            doc.text('3. Peraturan Presiden Republik Indonesia Nomor 8 tahun 2012 tentang Kerangka Kualifikasi Nasional Indonesia (KKNI) (Lembaran Negara Republik Indonesia Tahun 2012 Nomor 24);');
+            doc.text('4. Peraturan Menteri Pendidikan dan Kebudayaan Republik Indonesia Nomor 25 Tahun 2012 tentang Organisasi dan Tata Kerja Universitas Andalas (Berita Negara Republik Indonesia Tahun 2012 Nomor 434);');
+            doc.text('5. Peraturan Menteri Pendidikan dan Kebudayaan Republik Indonesia Nomor 47 Tahun 2013 tentang Statuta Universitas Andalas (Berita Negara Republik Indonesia Tahun 2013 Nomor 596);');
           } // Add more content as needed
           // Finalize the PDF and close the stream
 
