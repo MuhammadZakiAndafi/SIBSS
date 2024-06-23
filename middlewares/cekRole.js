d22437ac0278ba0a923574a17d7e12c7762e6159
reginanathamiya08 <reginanathamiya08@gmail.com>
2024-06-23 23:56:07 +0700

    Perbaikan riwayat

--- middlewares/cekRole.js
+++ middlewares/cekRole.js
@@ -4,8 +4,8 @@ function cekRole(req, res, next) {
   }
 
   const userRole = req.session.user.role;
-  const allowedRoutesForKaprodiWadek = ['/daftarPengajuan', 'riwayatpengajuan','/approval'];
-  const allowedRoutesForMahasiswa = ['/pendaftaranBss', '/status', '/riwayatpengajuan', '/panduan', '/suratkeputusan'];
+  const allowedRoutesForKaprodiWadek = ['/daftarPengajuan', '/riwayatpengajuan','/approval'];
+  const allowedRoutesForMahasiswa = ['/pendaftaranBss', '/status', '/riwayatpengajuanmahasiswa', '/panduan', '/suratkeputusan'];
   
   // Allow Kaprodi and Wadek to access detail routes
   const isDetailRoute = req.path.startsWith('/detail/');
